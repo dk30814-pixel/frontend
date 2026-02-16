b2aff459949ebc7caa20dbd982ab49b8c08b31b9
drinkurtishi <drin.kurtishi@hotelkeyapp.com>
2026-02-16 11:54:12 +0100

    fix actual url of backend

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL || 'https://food-recognition-backend-rzsl.onrender.com';
 
 function App() {
   const [capturing, setCapturing] = useState(false);
